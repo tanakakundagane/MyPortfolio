83164d8710f8c84be1328d33a978ea5ae88a98e3
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-06-17 21:43:43 +0900

    skilsへ画像の挿入

--- src/app/skils/page.tsx
+++ src/app/skils/page.tsx
@@ -11,10 +11,10 @@ const page = () => {
       <div className='flex-grow flex items-center justify-center'>
         <div className='w-[80%] mx-auto flex flex-col items-center md:flex-row-reverse md:gap-40 gap-20'>
           <SwingingImage
-            src='/images/image5.png'
+            src='/images/plants.png'
             width={400}
             height={400}
-            alt='Astro'
+            alt='plants'
           />
           <div>
             <div>
