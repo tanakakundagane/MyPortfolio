9d547660dace55b20e26a6b33df985c6b2e532a7
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-06-20 23:17:48 +0900

    imageの挿入

--- src/app/skils/page.tsx
+++ src/app/skils/page.tsx
@@ -1,116 +1,9 @@
-// import React from 'react';
-// import Header from '../components/Header';
-// import StarryBackground from '../components/StarryBackground';
-// import SwingingImage from '../components/SwingingImage';
-// import FadeIn, { FadeInStagger } from '../components/FadeIn';
-
-// const Page = () => {
-//   return (
-//     <div className="relative z-10 min-h-screen flex flex-col">
-//       <StarryBackground />
-//       <Header />
-//       <div className='w-[30%] mx-auto bg-black text-white text-center py-4 rounded-lg text-xl md:text-2xl mt-10'>
-//         <h1>MySkils</h1>
-//       </div>
-//       <div className="flex-grow flex items-center justify-center">
-//         <div className="w-[80%] mx-auto flex flex-col-reverse items-center md:flex-row md:gap-20 gap-20 mt-7">
-//           <FadeInStagger>
-//             <div className="flex-1 p-10 md:flex md:gap-10 border-2 md:justify-center bg-indigo-400 rounded-lg w-[100%]">
-//                 <ul className="text-center text-xl md:text-2xl">
-//                   <FadeIn>
-//                     <li className='pb-3'>HTML</li>
-//                   </FadeIn>
-//                   <FadeIn>
-//                     <li className='pb-3'>CSS</li>
-//                   </FadeIn>
-//                   <FadeIn>
-//                     <li className='pb-3'>Javascript</li>
-//                   </FadeIn>
-//                   <FadeIn>
-//                     <li className='pb-3'>React</li>
-//                   </FadeIn>
-//                   <FadeIn>
-//                     <li className='pb-3'>Resend</li>
-//                   </FadeIn>
-//                 </ul>
-//                 <ul className="text-center text-xl md:text-2xl">
-//                   <li className='pb-3'>Next.js</li>
-//                   <li className='pb-3'>Three.js</li>
-//                   <li className='pb-3'>framer-motion</li>
-//                   <li className=''>Node-mailer</li>
-//                 </ul>
-//             </div>
-//           </FadeInStagger>
-//           <div className="flex-1 md:flex md:justify-center">
-//             <SwingingImage
-//               src='/images/plants.png'
-//               width={400}
-//               height={400}
-//               alt='plants'
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Page;
-
-// import React from 'react';
-// import Header from '../components/Header';
-// import StarryBackground from '../components/StarryBackground';
-// import SwingingImage from '../components/SwingingImage';
-// import FadeIn, { FadeInStagger } from '../components/FadeIn';
-
-// const Page = () => {
-//   return (
-//     <div className="relative z-10 min-h-screen flex flex-col">
-//       <StarryBackground />
-//       <Header />
-//       <div className='w-[30%] mx-auto bg-black text-white text-center py-4 rounded-lg text-xl md:text-2xl mt-10'>
-//         <h1>My Skills</h1>
-//       </div>
-//       <div className="flex-grow flex items-center justify-center">
-//         <div className="w-[80%] mx-auto flex flex-col-reverse items-center md:flex-row md:gap-20 gap-20 mt-7">
-//           <FadeInStagger>
-//             <div className="flex-1 p-10 md:flex md:gap-10 border-2 md:justify-center bg-indigo-400 rounded-lg w-[100%]">
-//               <ul className="text-center text-xl md:text-2xl">
-//                 <FadeIn><li className='pb-3'>HTML</li></FadeIn>
-//                 <FadeIn><li className='pb-3'>CSS</li></FadeIn>
-//                 <FadeIn><li className='pb-3'>Javascript</li></FadeIn>
-//                 <FadeIn><li className='pb-3'>React</li></FadeIn>
-//                 <FadeIn><li className='pb-3'>Resend</li></FadeIn>
-//               </ul>
-//               <ul className="text-center text-xl md:text-2xl">
-//                 <FadeIn><li className='pb-3'>Next.js</li></FadeIn>
-//                 <FadeIn><li className='pb-3'>Three.js</li></FadeIn>
-//                 <FadeIn><li className='pb-3'>framer-motion</li></FadeIn>
-//                 <FadeIn><li className=''>Node-mailer</li></FadeIn>
-//               </ul>
-//             </div>
-//           </FadeInStagger>
-//           <div className="flex-1 md:flex md:justify-center">
-//             <SwingingImage
-//               src='/images/plants.png'
-//               width={400}
-//               height={400}
-//               alt='plants'
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Page;
-
 import React from 'react';
 import Header from '../components/Header';
 import StarryBackground from '../components/StarryBackground';
 import SwingingImage from '../components/SwingingImage';
 import FadeIn, { FadeInStagger } from '../components/FadeIn';
+import Image from 'next/image';
 
 const Page = () => {
   return (
@@ -121,32 +14,80 @@ const Page = () => {
         <h1>My Skills</h1>
       </div>
       <div className="flex-grow flex items-center justify-center pb-10">
-        <div className="w-[80%] mx-auto flex flex-col-reverse items-center md:flex-row md:gap-20 gap-20 mt-7">
+        <div className="w-[80%] mx-auto flex-col-reverse items-center md:flex-row md:gap-20 gap-20 mt-7">
           <FadeInStagger>
-            <div className="flex-1 p-10 md:flex md:gap-10 border-2 md:justify-center bg-indigo-400 rounded-lg w-[100%]">
+            <div className="flex-1 p-10 md:flex md:gap-20 border-2 md:justify-center rounded-lg w-[100%]">
               <ul className="text-center text-xl md:text-2xl text-shadow">
-                <FadeIn><li className='pb-3'>HTML</li></FadeIn>
-                <FadeIn><li className='pb-3'>CSS</li></FadeIn>
-                <FadeIn><li className='pb-3'>Javascript</li></FadeIn>
-                <FadeIn><li className='pb-3'>React</li></FadeIn>
-                <FadeIn><li className='pb-3'>Resend</li></FadeIn>
+                <FadeIn>
+                  <li className='pb-8 flex items-center gap-2'> 
+                    <Image src='/images/HTML5_Logo.png'width={50}height={50} alt='html'/>
+                    <p>HTML</p>
+                  </li>
+                </FadeIn>
+                <FadeIn>
+                  <li className='pb-8 flex items-center gap-2'>
+                    <Image src='/images/css.png' width={50}height={50} alt='css'/>
+                    <p>CSS</p>
+                  </li>
+                </FadeIn>
+                <FadeIn>
+                  <li className='flex items-center gap-2'>
+                    <Image src='/images/js.png' width={50}height={50} alt='javascript'/>
+                    <p>Javascript</p>
+                  </li>
+                </FadeIn>
+              </ul>
+
+              <ul className="text-center text-xl md:text-2xl text-shadow">
+                <FadeIn>
+                  <li className='pb-8 flex items-center gap-2'>
+                    <Image src='/images/react.png' width={50}height={50} alt='react'/>
+                    <p>React</p>
+                  </li>
+                </FadeIn>
+                <FadeIn>
+                  <li className='pb-8 flex items-center gap-2'>
+                    <Image src='/images/resend.png' width={50}height={50} alt='resend'/>
+                    <p>Resend</p>
+                  </li>
+                </FadeIn>
+                <FadeIn>
+                  <li className='flex items-center gap-2'>
+                    <Image src='/images/nextjs.png' width={50}height={50} alt='next.js'/>
+                    <p>Next.js</p>
+                  </li>
+                </FadeIn>
               </ul>
               <ul className="text-center text-xl md:text-2xl text-shadow">
-                <FadeIn><li className='pb-3'>Next.js</li></FadeIn>
-                <FadeIn><li className='pb-3'>Three.js</li></FadeIn>
-                <FadeIn><li className='pb-3'>framer-motion</li></FadeIn>
-                <FadeIn><li className=''>Node-mailer</li></FadeIn>
+                <FadeIn>
+                  <li className='pb-8 flex items-center gap-2'>
+                    <Image src='/images/Three.js.png' width={50}height={50} alt='three.js'/>
+                    <p>Three.js</p>
+                  </li>
+                </FadeIn>
+                <FadeIn>
+                  <li className='pb-8 flex items-center gap-2'>
+                    <Image src='/images/framer-motion.jpg' width={50}height={50} alt='framer-motion'/>
+                    <p>framer-motion</p>
+                  </li>
+                </FadeIn>
+                <FadeIn>
+                  <li className='flex items-center gap-2'>
+                    <Image src='/images/nodemailer.png' width={50}height={50} alt='nodemailer'/>
+                    <p>Node-mailer</p>
+                  </li>
+                </FadeIn>
               </ul>
             </div>
           </FadeInStagger>
-          <div className="flex-1 md:flex md:justify-center">
+          {/* <div className="flex-1 md:flex md:justify-center">
             <SwingingImage
               src='/images/plants.png'
               width={400}
               height={400}
               alt='plants'
             />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
